602abe13cab3c3ed3aec9e222960539fe4b07ccf
执着 <zhizhuoshuma@163.com>
2022-07-06 19:25:26 +0800

    笔记更新

--- docs/notes/物联网/sidebar_contents.js
+++ docs/notes/物联网/sidebar_contents.js
@@ -2,16 +2,16 @@
  * @Author: zhizhuo 
  * @Date: 2022-07-04 08:39:53 
  * @Last Modified by: zhizhuo
- * @Last Modified time: 2022-07-04 08:51:25
+ * @Last Modified time: 2022-07-06 19:06:11
  */
 
 module.exports = [
 	"/notes/物联网/",
 	{
-		title: "第一章 物联网概述",
+		title: "小李物联网笔记",
 		collapsable: true,
 		children: [
-			"/notes/物联网/book/第一章 物联网概述"
+			"/notes/物联网/book/第一章 物联网概述",
 		]
 	}
 ];
